037cc1a536f5ebc9c9d2a286e75eac66312e8009
PaperCube <imzhy@hotmail.com>
2018-09-30 20:53:16 +0800

    Number with suffix no longer fails to convert

--- src/studio/papercube/moossage/LocalProperties.java
+++ src/studio/papercube/moossage/LocalProperties.java
@@ -4,13 +4,13 @@ import java.io.File;
 import java.time.LocalDateTime;
 
 public final class LocalProperties {
-	public final static String version = "v1.2 Preview 4";
+	public final static String version = "v1.2 Preview 5";
 	public final static String author = "PaperCube";
-	public final static int build = 136;
-	public final static File resourceFolder(){
+	public final static int build = 137;
+	public static File resourceFolder(){
 		return new File("Moossage");
 	}
-	public final static String currentTimeSpecialized(){
+	public static String currentTimeSpecialized(){
 		LocalDateTime now = LocalDateTime.now();
 		return String.format("%d-%d-%d_%d-%d-%d", now.getYear(), now.getMonthValue(), now.getDayOfMonth(), now.getHour(), now.getMinute(), now.getSecond());
 	}
--- src/studio/papercube/moossage/utilities/SuffixUtil.java
+++ src/studio/papercube/moossage/utilities/SuffixUtil.java
@@ -1,61 +1,91 @@
 package studio.papercube.moossage.utilities;
 
 public class SuffixUtil {
+    private static class DoubleWithSuffix {
+        public static DoubleWithSuffix parse(String s) {
+            String sTrimmed = s.trim();
+            final int size = sTrimmed.length();
+            int digitEnd = size;
+            for (int i = 0; i < size; i++) {
+                char ch = sTrimmed.charAt(i);
+                if (!Character.isDigit(ch) && ch != '.') {
+                    digitEnd = i;
+                    break;
+                }
+            }
+            return new DoubleWithSuffix(
+                    Double.parseDouble(sTrimmed.substring(0, digitEnd)),
+                    sTrimmed.substring(digitEnd, size)
+            );
+        }
+
+        private double number;
+        private String suffix;
+
+        public double getNumber() {
+            return number;
+        }
+
+        public void setNumber(double number) {
+            this.number = number;
+        }
+
+        public String getSuffix() {
+            return suffix;
+        }
+
+        public void setSuffix(String suffix) {
+            this.suffix = suffix;
+        }
+
+        private DoubleWithSuffix(double number, String suffix) {
+            this.number = number;
+            this.suffix = suffix;
+        }
+    }
+
 	public static int toMillis(String formula){
-		String pure = formula.trim();
-		String suffix = String.valueOf(pure.charAt(pure.length()-1)).toLowerCase();
-		String numberString = pure.substring(0, pure.length()-1);
-		
-		if(pure.endsWith("ms")){
-			try{
-				return Integer.parseInt(pure.substring(0,pure.length()-2));
-			}catch(NumberFormatException e){
-				throw new InvalidSuffixException(e);
-			}
-		}
-		
-		try{
-			double baseNum = Double.parseDouble(numberString);
-			
+        try {
+            DoubleWithSuffix doubleWithSuffix = DoubleWithSuffix.parse(formula);
+            String suffix = doubleWithSuffix.getSuffix().toLowerCase();
+            double number = doubleWithSuffix.number;
 			switch(suffix){
-			case "s":
-				return (int)(baseNum*1000);
-			case "m":
-				return (int)(baseNum*1000*60);
-			case "h":
-				return (int)(baseNum * 1000 * 60 * 60);
-			default:
-				return Integer.parseInt(pure);
+                case "s":
+                    return (int) (number * 1000);
+                case "m":
+                    return (int) (number * 1000 * 60);
+                case "h":
+                    return (int) (number * 1000 * 60 * 60);
+                case "ms":
+                    return (int) number;
+                default:
+                    return (int) number;
 			}
 		}catch(NumberFormatException e){
 			throw new InvalidSuffixException(e);
 		}
-		
-	}
-	
+
+    }
+
 	public static int toTimes(String formula){
-		String pure = formula.trim();
-		String suffix = String.valueOf(pure.charAt(pure.length()-1)).toLowerCase();
-		String numberString = pure.substring(0, pure.length()-1);
-		
-		
-		try{
-			double baseNum = Double.parseDouble(numberString);
-			
-			switch(suffix){
-			case "k":
-				return (int)(baseNum*1000);
-			case "m":
-				return (int)(baseNum*1000*1000);
-			default:
-				return Integer.parseInt(pure);
+        try {
+            DoubleWithSuffix doubleWithSuffix = DoubleWithSuffix.parse(formula);
+            String suffix = doubleWithSuffix.getSuffix().toLowerCase();
+            double number = doubleWithSuffix.number;
+            switch (suffix) {
+                case "k":
+                    return (int) (number * 1000);
+                case "m":
+                    return (int) (number * 1000 * 1000);
+                default:
+                    return (int) number;
 			}
 		}catch(NumberFormatException e){
 			throw new InvalidSuffixException(e);
 		}
 	}
-	
-	public static class InvalidSuffixException extends RuntimeException{
+
+    public static class InvalidSuffixException extends RuntimeException {
 
 		private static final long serialVersionUID = 5881490402039607656L;
 
@@ -78,6 +108,8 @@ public class SuffixUtil {
 		public InvalidSuffixException(Throwable arg0) {
 			super(arg0);
 		}
-		
-	}
+
+    }
+
+
 }
